544637ebefe3b5a6f4f67b190ff8309fa91f6730
Eve <benlovesyellow@hotmail.com>
2026-02-05 22:57:27 -0500

    Create load-p5.js

---
+++ api/load-p5.js
@@ -0,0 +1,7 @@
+// p5.js CDN loader for Daemon Core Interface
+(function(){
+  var s=document.createElement('script');
+  s.src='https://cdn.jsdelivr.net/npm/p5@1.9.0/lib/p5.min.js';
+  s.async=false;
+  document.head.appendChild(s);
+})();
